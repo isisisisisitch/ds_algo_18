124173321f7ab03395f18c541ffcfd0251219362
isisisisisisithch <dallucus@gmail.com>
2022-10-17 22:35:26 -0400

    completed bst

--- src/ca/bytetube/_06_binarytree/BinarySearchTree.java
+++ src/ca/bytetube/_06_binarytree/BinarySearchTree.java
@@ -5,8 +5,7 @@ import java.util.Comparator;
 public class BinarySearchTree<E> extends BinaryTree<E> {
     protected Comparator<E> comparator;
 
-    public BinarySearchTree() {
-    }
+    public BinarySearchTree() { }
 
     public BinarySearchTree(Comparator<E> comparator) {
         this.comparator = comparator;
@@ -53,11 +52,58 @@ public class BinarySearchTree<E> extends BinaryTree<E> {
 
     // remove elements
     public void remove(E element) {
+        remove(node(element));
+    }
+
+
+    private void remove(Node<E> node) {
+        size--;
+        //1.Delete node-node with degree 2
+        if (node.hasTwoChildren()) {
+            Node<E> s = successor(node);
+            node.element = s.element;
+            node = s;
+        }
+        //2.Delete node-node with degree 1
+        Node<E> replacement = node.left != null ? node.left : node.right;
+        //双亲相认
+        if (replacement != null) {
+            replacement.parent = node.parent;
+            if (node.parent == null) root = replacement;
+            if (node == node.parent.left) node.parent.left = replacement;
+            else node.parent.right = replacement;
+        }
+
+        //3.Delete node-leaf node
+        else if (node.parent == null) root = null;
+
+        else {
+            if (node == node.parent.left) node.parent.left = null;
+            else node.parent.right = null;
+        }
 
     }
 
+
     // does it contain an element or not
     public boolean contains(E element) {
-        return false;
+        return node(element) != null;
+    }
+
+    private Node<E> node(E element) {
+        Node<E> node = root;
+        int cmp = 0;
+        while (node != null) {
+            cmp = compare(element, node.element);
+            if (cmp > 0) node = node.right;
+            else if (cmp < 0) node = node.left;
+            else {
+                node.element = element;
+                return node;
+            }
+
+        }
+
+        return null;
     }
 }
--- src/ca/bytetube/_06_binarytree/BinaryTree.java
+++ src/ca/bytetube/_06_binarytree/BinaryTree.java
@@ -1,15 +1,149 @@
 package ca.bytetube._06_binarytree;
 
+import ca.bytetube._06_binarytree.printer.BinaryTreeInfo;
+
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Stack;
 
-public class BinaryTree<E> {
+public class BinaryTree<E> implements BinaryTreeInfo {
     protected Node<E> root;
     protected int size;
 
+    public static abstract class Visitor<E> {
+        boolean stop;
+
+        public abstract boolean visit(E element);
+    }
+
+
+    public void preOrderTraversal() {
+        preOrderTraversal(root);
+    }
+
+    public void preOrderTraversal(Visitor<E> visitor) {
+        if (visitor == null) return;
+        preOrderTraversalByRecursion(root, visitor);
+    }
+
+    public void preOrderTraversalByRecursion(Node<E> head, Visitor<E> visitor) {
+        if (head == null || visitor.stop) return;
+        visitor.stop = visitor.visit(head.element);
+        preOrderTraversalByRecursion(head.left, visitor);
+        preOrderTraversalByRecursion(head.right, visitor);
+
+    }
+
+
+    /**
+     * 有右先压右，有左再压左
+     */
+    public void preOrderTraversal(Node<E> head) {
+        if (head == null) return;
+        Stack<Node<E>> stack = new Stack<>();
+        stack.push(head);
+        while (!stack.isEmpty()) {
+            Node<E> pop = stack.pop();
+
+            System.out.print(pop.element + " ");
+            if (pop.right != null) stack.push(pop.right);
+            if (pop.left != null) stack.push(pop.left);
+        }
+
+    }
+
+    public void inOrderTraversal() {
+        inOrderTraversalByRecursion(root);
+    }
+
+    public void inOrderTraversalByRecursion(Node<E> head) {
+        if (head == null) return;
+
+
+        inOrderTraversalByRecursion(head.left);
+        System.out.print(head.element + " ");
+        inOrderTraversalByRecursion(head.right);
+
+    }
+
+
+    public void inOrderTraversal(Node<E> head) {
+        if (head == null) return;
+        Stack<Node<E>> stack = new Stack<>();
+
+        if (head != null) {
+            while (!stack.isEmpty() || head != null) {
+                if (head != null) {
+                    stack.push(head);
+                    head = head.left;
+                } else {
+                    head = stack.pop();
+                    System.out.print(head.element + " ");
+                    head = head.right;
+                }
+
+            }
+
+        }
+
+
+    }
+
+    public void postOrderTraversal() {
+        postOrderTraversalByRecursion(root);
+    }
+
+    public void postOrderTraversalByRecursion(Node head) {
+        if (head == null) return;
+
+
+        postOrderTraversalByRecursion(head.left);
+
+        postOrderTraversalByRecursion(head.right);
+
+        System.out.print(head.element + " ");
+
+    }
+
+
+    public void postOrderTraversal(Node head) {
+        if (head == null) return;
+        Stack<Node<E>> stack = new Stack<>();
+        Stack<Node<E>> helpStack = new Stack<>();
+        stack.push(head);
+        while (!stack.isEmpty()) {
+            Node<E> pop = stack.pop();
+            helpStack.push(pop);
+            if (pop.left != null) stack.push(pop.left);
+            if (pop.right != null) stack.push(pop.right);
+
+        }
+
+        while (!helpStack.isEmpty()) System.out.print(helpStack.pop().element + " ");
+
+    }
+
+    public void levelOrderTraversal() {
+        levelOrderTraversal(root);
+    }
+
+    public void levelOrderTraversal(Node<E> head) {
+        if (head == null) return;
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.offer(head);
+        while (!queue.isEmpty()) {
+            Node<E> poll = queue.poll();
+            System.out.print(poll.element + " ");
+            if (poll.left != null) queue.offer(poll.left);
+            if (poll.right != null) queue.offer(poll.right);
+        }
+
+    }
+
+
     // number of elements
     public int size() {
-       return size;
+        return size;
     }
 
     // if it is empty or not
@@ -20,8 +154,42 @@ public class BinaryTree<E> {
     // clear all elements
     public void clear() {
         root = null;
+        size = 0;
     }
 
+
+    public Node<E> predecessor(Node<E> node) {
+        if (node == null) return null;
+        //1.node.left != null
+        Node<E> p = node.left;
+        if (p != null) {
+            while (p.right != null) p = p.right;
+
+            return p;
+        }
+        //2.node.left == null && node.parent != null
+        while (node.parent != null && node == node.parent.left) node = node.parent;
+        //3.node.left == null && node.parent == null
+        return node.parent;
+
+    }
+
+
+    public Node<E> successor(Node<E> node) {
+        if (node == null) return null;
+        //1.node.right != null
+        Node<E> p = node.right;
+        if (p != null) {
+            while (p.left != null) p = p.left;
+
+            return p;
+        }
+        //2.node.right == null && node.parent != null
+        while (node.parent != null && node == node.parent.right) node = node.parent;
+        //3.node.left == null && node.parent == null
+        return node.parent;
+
+    }
 
     public int height() {
         return height(root);
@@ -57,18 +225,21 @@ public class BinaryTree<E> {
         return height;
     }
 
+    public boolean isComplete() {
+        return isComplete(root);
+    }
 
-    public boolean isCompleteTree(Node root) {
-        if (root == null) return false;
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(root);
+    private boolean isComplete(Node<E> head) {
+        if (head == null) return false;
+        Queue<Node<E>> queue = new LinkedList<>();
+        queue.offer(head);
         boolean isLeaf = false;
         while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            if (isLeaf && !isLeafNode(node)) return false;
+            Node<E> node = queue.poll();
+            if (isLeaf && !node.isLeafNode()) return false;
             //1. If node.left!=null && node.right!=null,
             // add node.left and node.left to the queue in sequence
-            if (hasTwoChildren(node)) {
+            if (node.hasTwoChildren()) {
                 queue.offer(node.left);
                 queue.offer(node.right);
             }
@@ -87,13 +258,45 @@ public class BinaryTree<E> {
         return true;
     }
 
-    private boolean hasTwoChildren(Node node) {
-        return node.left != null && node.right != null;
+    public boolean isBalanced() {
+
+        return isBalanced(root);
     }
 
-    private boolean isLeafNode(Node node) {
-        return node.left == null && node.right == null;
+
+    public boolean isBalanced(Node<E> head) {
+
+        return false;
     }
+
+
+
+    @Override
+    public Object root() {
+        return root;
+    }
+
+    @Override
+    public Object left(Object node) {
+        return ((Node<E>) node).left;
+    }
+
+    @Override
+    public Object right(Object node) {
+        return ((Node<E>) node).right;
+    }
+
+    @Override
+    public Object string(Object node) {
+        Node<E> myNode = (Node<E>) node;
+        String parentString = "null";
+        if (myNode.parent != null) {
+            parentString = myNode.parent.element.toString();
+
+        }
+        return myNode.element + "_p(" + parentString + ")";
+    }
+
 
     public static class Node<E> {
         protected E element;
@@ -102,11 +305,18 @@ public class BinaryTree<E> {
         protected Node<E> parent;
 
 
-        public Node(E val,Node parent) {
+        public Node(E val, Node parent) {
             this.element = val;
             this.parent = parent;
         }
 
+        protected boolean hasTwoChildren() {
+            return this.left != null && this.right != null;
+        }
+
+        protected boolean isLeafNode() {
+            return this.left == null && this.right == null;
+        }
 
 
         @Override
